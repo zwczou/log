3631680820dbaff88aa2145ad2fc82ca67f0af0a
zwc <zwczou@gmail.com>
2018-04-20 15:43:54 +0800

    完善测试日志

--- log_test.go
+++ log_test.go
@@ -8,8 +8,28 @@ import (
 func Test_Logger(t *testing.T) {
 	buff := bytes.NewBufferString("")
 	log := New(buff, "", 0)
+
+	buff.Reset()
+	log.Debugf("debug")
+	if buff.String() != "DEBUG: debug\n" {
+		t.Fatalf("want %s got %s", "DEBUG: debug", buff.String())
+	}
+
+	buff.Reset()
 	log.Infof("hello world!")
 	if buff.String() != "INFO: hello world!\n" {
-		log.Fatal("want %s got %s", "INFO: hello world1", buff.String())
+		t.Fatalf("want %s got %s", "INFO: hello world1", buff.String())
+	}
+
+	buff.Reset()
+	log.Warnf("warn")
+	if buff.String() != "WARN: warn\n" {
+		t.Fatalf("want %s got %s", "WARN: warn", buff.String())
+	}
+
+	buff.Reset()
+	log.Errorf("e")
+	if buff.String() != "ERROR: e\n" {
+		t.Fatalf("want %s got %s", "ERROR: e", buff.String())
 	}
 }
